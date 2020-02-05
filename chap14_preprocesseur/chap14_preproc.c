d16f162fd86c55eb23a1ff3b13d72fbb8d8dab5d
arkenopterix <briegbk@yahoo.com>
2020-02-05 22:09:14 +0100

    added preproc tuto

---
+++ chap14_preprocesseur/chap14_preproc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+
+}
+//gcc -Wall -c "chap14_strings.c"
+//gcc -Wall -o "chap14_strings" "chap14_strings.c"
