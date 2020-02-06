4d85d7e1410eb2cc3c5aaff6914966d377b41659
arkenopterix <briegbk@yahoo.com>
2020-02-06 23:14:11 +0100

    added chap15 structs

---
+++ chap15_structs/chap15_structs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "chap15_structs.h"
+
+int main(int argc, char *argv[])
+{
+
+    printf("Aire calculee via define : %d \n",AIRE);
+
+
+
+	return 0;
+
+}
+//gcc -Wall -c "chap14_strings.c"
+//gcc -Wall -o "chap14_strings" "chap14_strings.c"
---
+++ chap15_structs/chap15_structs.h
@@ -0,0 +1,12 @@
+#ifndef CHAP15_STRUCTS
+define CHAP15_STRUCTS
+
+
+typedef struct Coordonnees Coordonnees;
+struct Coordonnees
+{
+    int x;
+    int y;
+};
+
+#endif
