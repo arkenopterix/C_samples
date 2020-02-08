d93aa7b721d7a75c8399435d222db19a8ae6c5a6
arkenopterix <briegbk@yahoo.com>
2020-02-08 16:46:53 +0100

    updated chap15 structs

--- chap15_structs/chap15_structs.c
+++ chap15_structs/chap15_structs.c
@@ -2,11 +2,41 @@
 #include <stdlib.h>
 #include "chap15_structs.h"
 
+void initialiserJoueur(Joueur *j)
+{
+    printf("init joueur 2\n");
+    sprintf(j->nom,"joueur2");
+    j->vies = 2;
+    j->f=NORMAL;
+
+}
+
+
 int main(int argc, char *argv[])
 {
 
-    printf("Aire calculee via define : %d \n",AIRE);
+    Joueur jTab[3];
+    Joueur *j3 = &(jTab[2]);
 
+    jTab[0] = (Joueur) {"",0,BALAISE}; // initialisation de la structure
+
+    sprintf(jTab[0].nom,"joueur1");
+    jTab[0].vies=3;
+    printf("J1 nom : %s \n",jTab[0].nom);
+    printf("J1 nombre de vies : %d \n",jTab[0].vies);
+    printf("J1 Force: %d \n",jTab[0].f);
+
+    initialiserJoueur(&(jTab[1]));
+    printf("J2 nom : %s \n",jTab[1].nom);
+    printf("J2 nombre de vies : %d \n",jTab[1].vies);
+    printf("J2 Force: %d \n",jTab[1].f);
+
+    sprintf(j3->nom,"joueur3");
+    j3->vies = 4;
+    j3->f=QUICHE;
+    printf("J3 nom : %s \n",j3->nom);
+    printf("J3 nombre de vies : %d \n",j3->vies);
+    printf("J3 Force: %d \n",j3->f);
 
 
 	return 0;
--- chap15_structs/chap15_structs.h
+++ chap15_structs/chap15_structs.h
@@ -1,12 +1,35 @@
 #ifndef CHAP15_STRUCTS
-define CHAP15_STRUCTS
+#define CHAP15_STRUCTS
 
+//on peut créer un enum de la manière suivante (le compilateur assigne des nombre au pif aux valeurs)
+typedef enum Force Force;
+enum Force
+{
+    QUICHE, NORMAL, BALAISE
+};
 
-typedef struct Coordonnees Coordonnees;
-struct Coordonnees
+// on peut aussi forcer le compilateur a prendre nos valeurs
+typedef enum Volume Volume;
+enum Volume
+{
+    FAIBLE=10,MOYEN=50,FORT=100
+};
+
+typedef struct Coordonnees Coordonnees;// permet de ne pas avoir a mettre "struct"
+struct Coordonnees // definit la structure
 {
     int x;
     int y;
 };
+
+typedef struct Joueur Joueur;
+struct Joueur
+{
+    char nom[20];
+    int vies;
+    Force f;
+};
+
+
 
 #endif
