f83350b3f13f5233f7868c9fbcbf428312439237
arkenopterix <briegbk@yahoo.com>
2020-08-12 14:51:48 +0200

    Migrated the menu screen and made it working with a mouse

--- TP_Sokoban/src/menu.c
+++ TP_Sokoban/src/menu.c
@@ -13,7 +13,7 @@ int menu(SDL_Surface *ecran)
     printf("Menu debut\n");
 #endif // DEBUG
     int retour = 0, continuer= 1;
-    SDL_Rect menuPos;
+    SDL_Rect menuPos, mousePos;
     int choixJeu = MENU;// variable pour la seletion du menu
     SDL_Event event;
 
@@ -24,12 +24,13 @@ int menu(SDL_Surface *ecran)
     menuPos.y=0;
     SDL_BlitSurface(menuIMG, NULL, ecran, &menuPos);
     SDL_Flip(ecran); // on met à jour l'affichage
+#ifdef DEBUG
+        printf("Menu -- Boucle principale\n");
+#endif
 	// boucle infinie
 	while(choixJeu != QUITTER) //tant que l'on a pas reussi a mettre les caisse sur tous les objectifs
 	{
-#ifdef DEBUG
-	    printf("Menu -- Boucle principale\n");
-#endif
+
 		//on gere les events
 		SDL_WaitEvent(&event); // Récupération de l'evenement dans event
         switch(event.type)
@@ -67,10 +68,44 @@ int menu(SDL_Surface *ecran)
                         return EDITEUR;
 
                         break;
+                    
 
                     default:
                     	break;
 
+                }
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                if (event.button.x>110 && event.button.x<285)
+                {
+#ifdef DEBUG
+                    printf("X:%d\n",event.button.x);
+#endif
+                    if(event.button.y>230 && event.button.y<270)
+                    {
+#ifdef DEBUG
+                        printf("JEU y:%d\n",event.button.y);
+#endif
+                        return JEU;
+                    }
+                    else if(event.button.y>286 && event.button.y<330)
+                    {
+#ifdef DEBUG
+                        printf("EDITEUR y:%d\n",event.button.y);
+#endif
+                        return EDITEUR;
+                    }
+                    else if(event.button.y>340 && event.button.y<380)
+                    {
+#ifdef DEBUG
+                        printf("QUITTER y:%d\n",event.button.y);
+#endif
+                        return QUITTER;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
                 break;
            	default:
